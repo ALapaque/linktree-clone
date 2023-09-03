4c85cb4e15608e29975be01250dccafb7f104ecf
amaury <amaury.lapaque@gmail.com>
2023-09-03 12:25:53 +0200

    testing

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default defineNuxtPlugin((NuxtApp) => {
-    axios.defaults.baseURL = 'https://api-linktree.amaury-lapaque.io'
+    axios.defaults.baseURL = 'https://api-linktree.amaury-lapaque.io:8000'
     axios.defaults.withCredentials = true;
 
     return {
